89a9c48cf92bb2bb206647f78231d740b12d7cb1
Alex Winter <ajwinter42@gmail.com>
2025-04-29 15:39:53 +0100

    a

--- client/components/Layers/LayerListing.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "Service/Component";
-
-export class LayerListing extends Component
-{
-    protected build(): HTMLElement {
-        throw new Error("Method not implemented.");
-    }
-
-}
