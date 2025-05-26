7f23e2336c1090689c71ec618ec2ca75379ea0d9
Alex Winter <ajwinter42@gmail.com>
2025-05-26 15:48:55 +0100

    a

---
+++ src/Client/Component/PlacementHistory/PlacementHistory.ts
@@ -0,0 +1,12 @@
+import { Component } from 'Client/Service/Component'
+import { Dom } from 'Client/Service/Dom'
+
+class PlacementHistory extends Component {
+    protected build(): HTMLElement {
+        const container = Dom.div()
+
+
+
+        return container
+    }
+}
